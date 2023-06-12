45414e3f4f0365ade9e1761edc2afce189936e84
Timo <ttensplus@gmail.com>
2023-06-12 22:49:48 +0300

    add main page and nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Timo Klimov</a>
+        <div class="nav-links">
+            <a href="/about" class="link">About me</a>
+            <a href="/blog" class="link">Blog</a>
+            <a href="/projects" class="link">My projects</a>
+            <a href="/contacts" class="link">Contacts</a>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
